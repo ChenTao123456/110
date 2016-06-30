2dee8e2c39fa2ebe72ca3d8124b8648a97fed717
张三 <775315669@qq.com>
2016-06-30 16:50:55 +0800

    输出

---
+++ biachi/biachi/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  biachi
+//
+//  Created by mac on 16/6/30.
+//  Copyright © 2016年 Earl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"白痴");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
